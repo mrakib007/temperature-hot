edbf88493d8184acc9715243a4313d48db351d6d
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-05 21:01:08 +0600

    solved some problems

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-  // my key  32836f6b257f319cf67d3d85356fc7d1
+  //my key  32836f6b257f319cf67d3d85356fc7d1
     
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=32836f6b257f319cf67d3d85356fc7d1')
-  .then(res=>res.json())
-  .then(data=>{
-      let temp = getElementById('temperature');
-      let kelvin = data.main.temp;
-      let celsius = kelvin-273.15;
-      temp.innerText = celsius;
-  })
+//   fetch('https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=32836f6b257f319cf67d3d85356fc7d1')
+//   .then(res=>res.json())
+//   .then(data=>{
+//       let temp = getElementById('temperature');
+//       let kelvin = data.main.temp;
+//       let celsius = kelvin-273.15;
+//       temp.innerText = celsius;
+//   })
 
 function weatherChecker(){
   let cityInput = document.getElementById('city-input').value;
@@ -25,7 +25,7 @@ function weatherChecker(){
           let celcius = (kelvin - 273.15).toFixed(1);
           temperature.innerText = celcius;
           city.innerText = cityInput;
-          let weatherNow = weather[0].main;
+          let weatherNow = data.weather[0].main;
           document.getElementById('cloud').innerText = weatherNow;
       }
   })
